6a7c9a0159dfbaa8b77d561f84077404927b3a71
AnasAlsadany <148365426+AnasAlsadany@users.noreply.github.com>
2024-02-27 19:47:41 +0200

    change create endpoint

--- src/app/@core/api/api-config/security.ts
+++ src/app/@core/api/api-config/security.ts
@@ -1,7 +1,7 @@
 export const Security_API = {
     "security":{
         "get":"Law_Role/GetList",
-        "create":"Law_Role/NewRole",
+        "create":"Law_Role/Create",
         "update":"Law_Role/Update",
         "delete":"Law_Role/Delete",
         "getById":"Law_Role/GetRoleById",
